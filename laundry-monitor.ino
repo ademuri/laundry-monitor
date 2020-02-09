6c29ad7853d2339d47bf44e5289c5a868b100309
Adam Demuri <adam.demuri@gmail.com>
2020-02-08 17:57:13 -0700

    Switch to using median filters for the sensor.

--- laundry-monitor.ino
+++ laundry-monitor.ino
@@ -2,10 +2,14 @@
 #include "digicert.h"
 #include <WiFi.h>
 #include "twilio.hpp"
+#include "median-filter.h"
 
 // Note: must use ADC1, ADC2 becomes unusable during Wifi usage
 static const uint8_t kCurrentPin = 33;
 static const uint8_t kLedPin = 2;
+
+MedianFilter<uint32_t, uint32_t, 5> *easy_filter;
+MedianFilter<uint32_t, uint32_t, 201> *hard_filter;
 
 Twilio *twilio;
 
@@ -13,6 +17,14 @@ void setup() {
   Serial.begin(115200);
 
   pinMode(kCurrentPin, INPUT);
+  // Require 3 on samples to stay on
+  easy_filter = new MedianFilter<uint32_t, uint32_t, 5>(filter_functions::ForAnalogRead<kCurrentPin>());
+  easy_filter->SetMinRunInterval(5);
+
+  // Require 10s of on before turning on
+  hard_filter = new MedianFilter<uint32_t, uint32_t, 201>(filter_functions::ForAnalogRead<kCurrentPin>());
+  hard_filter->SetMinRunInterval(100);
+
   pinMode(kLedPin, OUTPUT);
 
   digitalWrite(kLedPin, LOW);
@@ -27,54 +39,49 @@ void setup() {
   twilio = new Twilio(account_sid, auth_token, DigiCertGlobalRootCA_crt);
 }
 
-int32_t kCycleDelayMs = 10;
-int32_t kOnToOffThreshold = 2 * 60 * 1000 / kCycleDelayMs; // Two minutes
-int32_t kOffToOnThreshold = 10 * 1000 / kCycleDelayMs; // 10 seconds
+//int32_t kCycleDelayMs = 10;
+int32_t kOnToOffThreshold = 150 * 1000; // 2.5 minutes
+int32_t kOffToOnThreshold = 10 * 1000; // 10 seconds
 
-uint32_t average;
 bool laundryOn = false;
-int32_t changeCount = 0;
-int printCount = 0;
+bool sensor_state = false;
+uint32_t change_at = 0;
+uint32_t print_at = 0;
 
 void loop() {
-  average = (average * 3 + analogRead(kCurrentPin)) / 4;
+  easy_filter->Run();
+  hard_filter->Run();
+
   if (laundryOn) {
-    if (average == 0) {
-      changeCount++;
-      if (changeCount > kOnToOffThreshold) {
-        laundryOn = false;
-        changeCount = 0;
-        digitalWrite(kLedPin, false);
-        // DING
-        String message = "Washer is done";
-        String response;
-        bool success = twilio->send_message(to_number, from_number, message, response);
-        if (success) {
-          Serial.println("Sent message successfully");
-        } else {
-          Serial.println(response);
-        }
+    if (easy_filter->GetFilteredValue() > 0) {
+      change_at = millis() + kOnToOffThreshold;
+    } else if (millis() > change_at) {
+      laundryOn = false;
+      sensor_state = false;
+      digitalWrite(kLedPin, false);
+      // DING
+      String message = "Washer is done";
+      String response;
+      bool success = twilio->send_message(to_number, from_number, message, response);
+      if (success) {
+        Serial.println("Sent message successfully");
+      } else {
+        Serial.println(response);
       }
-    } else if (changeCount > 0) {
-      changeCount -= 10;
     }
   } else {
-    if (average > 0) {
-      changeCount++;
-      if (changeCount > kOffToOnThreshold) {
-        laundryOn = true;
-        changeCount = 0;
-        digitalWrite(kLedPin, true);
-      }
-    } else if (changeCount > 0) {
-      changeCount--;
+    // laundryOn == false
+    if (hard_filter->GetFilteredValue() > 0) {
+      laundryOn = true;
+      digitalWrite(kLedPin, true);
+      change_at = millis() + kOnToOffThreshold;
     }
   }
 
-  printCount++;
-  if (printCount > 10) {
-    printCount = 0;
-    Serial.println(average);
+  if (millis() > print_at) {
+    Serial.print(easy_filter->GetFilteredValue());
+    Serial.print(" ");
+    Serial.println(laundryOn ? "on" : "off");
+    print_at = millis() + 500;
   }
-  delay(kCycleDelayMs);
 }
