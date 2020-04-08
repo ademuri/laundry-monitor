935896933661f6bbb24dd343050809bd54a9e2b0
Adam Demuri <adam.demuri@gmail.com>
2020-04-07 21:08:44 -0600

    Add ArduinoOTA

--- laundry-monitor/laundry-monitor.ino
+++ laundry-monitor/laundry-monitor.ino
@@ -1,3 +1,5 @@
+#include <ArduinoOTA.h>
+#include <SPIFFS.h>
 #include <WiFi.h>
 #include <functional>
 #include <vector>
@@ -56,9 +58,40 @@ void setup() {
   }
 
   twilio = new Twilio(account_sid, auth_token);
+
+  ArduinoOTA
+    .onStart([]() {
+      String type;
+      if (ArduinoOTA.getCommand() == U_FLASH) {
+        type = "sketch";
+      } else { // U_SPIFFS
+        type = "filesystem";
+      }
+
+      Serial.println("Start updating " + type);
+      SPIFFS.end();
+    })
+    .onEnd([]() {
+      Serial.println("\nEnd");
+    })
+    .onProgress([](unsigned int progress, unsigned int total) {
+      Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+    })
+    .onError([](ota_error_t error) {
+      Serial.printf("Error[%u]: ", error);
+      if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
+      else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
+      else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
+      else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
+      else if (error == OTA_END_ERROR) Serial.println("End Failed");
+    });
+
+  ArduinoOTA.begin();
 }
 
 void loop() {
+  ArduinoOTA.handle();
+
   if (calibrate) {
     Serial.print(analogRead(kWasherPin));
     Serial.print(" ");
